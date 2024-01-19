0d3f2223e09160b200d8148f878e70c9a96ebc29
SRDhanush16 <dhanushsr0616@gmail.com>
2024-01-19 18:58:07 +0530

    CSS update 1

--- src/MainPageFD/Feed.js
+++ src/MainPageFD/Feed.js
@@ -5,24 +5,6 @@ import React, { useState, useEffect } from 'react';
 import { db } from "../firebase-config";
 import { collection,getDocs } from "firebase/firestore";
 
-
-let arrobj = [
-    {
-        "name_of_item" : "casio black color Analog watch",
-        "picture-url" : " images/feed/watchjpg.jpg",
-        "reward" : " 300",
-        "desc" : " I lost my casio watch near AB1 ground floor fluid dynamics laboratory steps , In case anyone finds pls contact : 1234567890" 
-    },
-
-    {
-        "name_of_item" : "Samsung purple color earbuds 2021 edition",
-        "picture-url" : " images/feed/earbuds.jpg",
-        "reward" : " 600",
-        "desc" : " I lost my Samsung purple color earbuds 2021 edition near AB1 ground floor fluid dynamics laboratory steps , In case anyone finds pls contact : 1234567890" 
-    }
-
-]
-
 function Feed() {
     const [data, setData] = useState([]);
   
@@ -33,15 +15,14 @@ function Feed() {
             const dataSnapshot = await getDocs(dataRef);
             const newData = dataSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
             setData(newData);
-            //console.log(data);
           } catch (error) {
             console.error('Error fetching data:', error);
           }
       };
+        fetchData();
+    }, []); 
   
-      fetchData();
-    }, []); // Make sure to close the useEffect dependencies array
-  
+    // checking if data is received
     useEffect(() => {
         console.log(data);
       }, [data]);
@@ -52,20 +33,37 @@ function Feed() {
         <section className="back-color">
           <div className="container">
             <div className="Feed">
-              {data.map((objarr) => (
+              {/* {data.map((objarr) => (
                 <div className="Post-post" key={objarr.id}>
                   <div className="Post-details">
-                    <h2>{objarr.name_of_item}</h2>
+                    <h2>{objarr.name}</h2>
                   </div>
                   <div className="Post-image">
-                    <img src={objarr["picture-url"]} alt={objarr.name_of_item} />
+                    <img src={objarr.imageUrl} alt={objarr.name} />
                     <h1>Reward: {objarr.reward}</h1>
                   </div>
                   <div className="Post-desc">
-                    <p>{objarr.desc}</p>
+                    <p>{objarr.description}</p>
                   </div>
                 </div>
-              ))}
+              ))} */}
+              {data.map((objarr) => (
+              <div
+                className={`Post-post ${objarr.type === 'Found' ? 'found-post' : ''}`}
+                key={objarr.id}
+              >
+                <div className="Post-details">
+                  <h2>{objarr.name}</h2>
+                </div>
+                <div className="Post-image">
+                  <img src={objarr.imageUrl} alt={objarr.name} />
+                  {objarr.type === 'Lost' && <h1>Reward: {objarr.reward}</h1>}
+                </div>
+                <div className="Post-desc">
+                  <p>{objarr.description}</p>
+                </div>
+              </div>
+            ))}
             </div>
           </div>
         </section>
