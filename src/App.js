0599baa8687e9f6d90eda513330ada425fac0271
Pratheepx <meetvpratheep@gmail.com>
2024-01-28 19:07:04 +0530

    small changes for deployment 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from "./LoginPageFD/LoginPage";
 import Myaccount from "./MainPageFD/Myaccount";
 import Feed from "./MainPageFD/Feed";
